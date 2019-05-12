8c29500eef2e76ba67ca4c1c877d3234325313da
InsomniaNoir <jrshafer@mac.com>
2019-05-12 18:24:29 -0500

    Integrated new and create actions for folios

--- app/controllers/folios_controller.rb
+++ app/controllers/folios_controller.rb
@@ -2,4 +2,27 @@ class FoliosController < ApplicationController
   def index
     @folio_items = Folio.all
   end
+
+  def new
+    @folio_item = Folio.new
+  end
+
+  def create
+    @folio_item = Folio.new(folio_item_params)
+
+    respond_to do |format|
+      if @folio_item.save
+        format.html{ redirect_to folios_path, notice: 'Your folio has been created' }
+      else
+        format.html { render :new }
+      end
+    end
+
+  end
+
+  private
+
+  def folio_item_params
+    params.require(:folio).permit(:title, :subtitle, :body)
+  end
 end
