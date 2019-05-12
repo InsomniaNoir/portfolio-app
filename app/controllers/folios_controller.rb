eaedff2934e82099f329eac38d823f16d3b02b63
InsomniaNoir <jrshafer@mac.com>
2019-05-12 18:40:56 -0500

    Added edit/create/update functionality to Portfolios

--- app/controllers/folios_controller.rb
+++ app/controllers/folios_controller.rb
@@ -12,12 +12,26 @@ class FoliosController < ApplicationController
 
     respond_to do |format|
       if @folio_item.save
-        format.html{ redirect_to folios_path, notice: 'Your folio has been created' }
+        format.html { redirect_to folios_path, notice: 'Your portfolio has been created' }
       else
         format.html { render :new }
       end
     end
 
+  end
+
+  def edit
+    @folio_item = Folio.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      if @folio_item.update(folio_item_params)
+        format.html { redirect_to folios_path, notice: 'Your portfolio has been updated' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   private
