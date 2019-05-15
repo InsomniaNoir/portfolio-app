a1a96a03880bb774f7dead103c2ecdcdff0d8877
InsomniaNoir <jrshafer@mac.com>
2019-05-15 17:39:16 -0500

    Configured whitelist

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -1,6 +1,7 @@
 module DeviseWhitelist
   extend ActiveSupport::Concern
 
+  # Changed to before_action. before_filter has been deprecated
   included do
     before_action :configure_permitted_paramaters, if: :devise_controller?
   end
