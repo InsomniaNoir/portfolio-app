f923c3741001fb86bb59b7b789fdafd917d8d234
InsomniaNoir <jrshafer@mac.com>
2019-05-15 18:54:04 -0500

    Added guest_user for current_user

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -7,8 +7,8 @@ module DeviseWhitelist
   end
 
   def configure_permitted_paramaters
-    devise_paramater_sanitizer.permit(:sign_up, keys: [:name])
-    devise_paramater_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
 end
