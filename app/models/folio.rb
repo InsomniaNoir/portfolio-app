ae1e888fe9143e537e201198ecbe8e1f1b6de39b
InsomniaNoir <jrshafer@mac.com>
2019-05-15 14:59:49 -0500

    Implemented Scopes for folios

--- app/models/folio.rb
+++ app/models/folio.rb
@@ -1,3 +1,11 @@
 class Folio < ApplicationRecord
-  validates_presence_of :title, :body, :main_image, :thumb_image 
+  validates_presence_of :title, :body, :main_image, :thumb_image
+
+  # Pure Ruby Scope
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  # Lambda Scope
+  scope ruby_on_rails_folio_items, -> { where(subtitle: 'Ruby on Rails') }
 end
