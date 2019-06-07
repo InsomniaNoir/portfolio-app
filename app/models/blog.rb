f431cd164be3459d929c4f0eb939d4b3196a6993
InsomniaNoir <jrshafer@mac.com>
2019-06-07 15:42:38 -0500

    Added custom scope for recent blogs and hide posts from non-admins

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,16 @@ class Blog < ApplicationRecord
 
   validates_presence_of :title, :body
   belongs_to :topic
+
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
+
+  def self.recent
+    order("created_at DESC")
+  end
 end
